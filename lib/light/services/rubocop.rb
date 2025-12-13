1167edad9f104f603e5ccf67d4bd08fd84ba2578
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2025-12-13 15:42:28 +0000

    Add new Rubocop cops ConditionMethodExists, DslOrder, MissingPrivateKeyword and NoDirectInstantiation

--- lib/light/services/rubocop.rb
+++ lib/light/services/rubocop.rb
@@ -3,5 +3,9 @@
 require "rubocop"
 
 require_relative "rubocop/cop/light_services/argument_type_required"
+require_relative "rubocop/cop/light_services/condition_method_exists"
+require_relative "rubocop/cop/light_services/dsl_order"
+require_relative "rubocop/cop/light_services/missing_private_keyword"
+require_relative "rubocop/cop/light_services/no_direct_instantiation"
 require_relative "rubocop/cop/light_services/output_type_required"
 require_relative "rubocop/cop/light_services/step_method_exists"
